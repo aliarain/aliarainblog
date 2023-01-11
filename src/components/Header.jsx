a0d3628fa83c3fcd09a978bdab97c330d7a781d9
Ali Arain <deistali2@gmail.com>
2023-01-11 23:17:07 +0500

    notes directory updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -121,11 +121,11 @@ function MobileNavigation(props) {
             </div>
             <nav className="mt-6">
               <ul className="-my-2 divide-y divide-zinc-100 text-base text-zinc-800 dark:divide-zinc-100/5 dark:text-zinc-300">
-                <MobileNavItem href="/about">About</MobileNavItem>
-                <MobileNavItem href="/articles">Articles</MobileNavItem>
-                <MobileNavItem href="/projects">Projects</MobileNavItem>
-                <MobileNavItem href="/podcast">Podcast</MobileNavItem>
-                <MobileNavItem href="/stack">Stack</MobileNavItem>
+                <MobileNavItem href="/about">about</MobileNavItem>
+                <MobileNavItem href="/notes">notes</MobileNavItem>
+                <MobileNavItem href="/projects">projects</MobileNavItem>
+                <MobileNavItem href="/podcast">podcast</MobileNavItem>
+                <MobileNavItem href="/stack">stack</MobileNavItem>
               </ul>
             </nav>
           </Popover.Panel>
