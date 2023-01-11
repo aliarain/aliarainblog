9e20417a7f3a0cd0ae1e858d4d67a5e5793bb778
Ali Arain <deistali2@gmail.com>
2023-01-11 23:20:20 +0500

    nav edit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -162,11 +162,11 @@ function DesktopNavigation(props) {
   return (
     <nav {...props}>
       <ul className="flex rounded-full bg-white/90 px-3 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10">
-        <NavItem href="/about">About</NavItem>
-        <NavItem href="/articles">Articles</NavItem>
-        <NavItem href="/projects">Projects</NavItem>
-        <NavItem href="/podcast">Podcast</NavItem>
-        <NavItem href="/stack">Stack</NavItem>
+        <NavItem href="/about">about</NavItem>
+        <NavItem href="/notes">notes</NavItem>
+        <NavItem href="/projects">projects</NavItem>
+        <NavItem href="/podcast">podcast</NavItem>
+        <NavItem href="/stack">stack</NavItem>
       </ul>
     </nav>
   )
