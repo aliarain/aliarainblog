c0a8ff007f29f0f3d9f1403519f53f592242b411
Ali Arain <deistali2@gmail.com>
2023-01-15 18:38:22 +0500

    minor 1

--- src/pages/stack.jsx
+++ src/pages/stack.jsx
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-
+import Image from 'next/image'
 import { Card } from '@/components/Card'
 import { Section } from '@/components/Section'
 import { SimpleLayout } from '@/components/SimpleLayout'
