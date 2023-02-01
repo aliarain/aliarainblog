d4303e95d2158acba10a8ae87824bf38e27dbc91
Ali Arain <deistali2@gmail.com>
2023-02-01 23:15:13 +0500

    llm

--- src/pages/future.jsx
+++ src/pages/future.jsx
@@ -35,13 +35,15 @@ export default function Uses() {
       <SimpleLayout
         title="the future i'm building."
         intro="this pages is a list of things i'm currently building or have a plan to build a them.
-        most of my future startups will be backed generative machine learning models."
+        most of my future startups will be backed by open source generative machine learning models."
       >
         <div className="space-y-20">
-          <Tool title="Flutter">
-            One of my favourite framework to develop apps with. I have been
-            using Flutter for over 4 years now and its really productive and fun
-            to develop app with.
+          <Tool title="LLM">
+            Future of software is working with large language models (LLM) and
+            making sure the end-user uses it safely and model solve their. Such
+            as developing a custom model on stable diffusion to automate the
+            products images or building a model which generates codes or whole
+            app in from just one prompt.
           </Tool>
           <Tool title="Apple Pro Display XDR (Standard Glass)">
             The only display on the market if you want something HiDPI and
@@ -62,52 +64,6 @@ export default function Uses() {
             If Im going to slouch in the worst ergonomic position imaginable all
             day, I might as well do it in an expensive chair.
           </Tool>
-
-          <ToolsSection title="Development tools">
-            <Tool title="Sublime Text 4">
-              I dont care if its missing all of the fancy IDE features everyone
-              else relies on, Sublime Text is still the best text editor ever
-              made.
-            </Tool>
-            <Tool title="iTerm2">
-              Im honestly not even sure what features I get with this that
-              aren’t just part of the macOS Terminal but its what I use.
-            </Tool>
-            <Tool title="TablePlus">
-              Great software for working with databases. Has saved me from
-              building about a thousand admin interfaces for my various projects
-              over the years.
-            </Tool>
-          </ToolsSection>
-          <ToolsSection title="Design">
-            <Tool title="Figma">
-              We started using Figma as just a design tool but now its become
-              our virtual whiteboard for the entire company. Never would have
-              expected the collaboration features to be the real hook.
-            </Tool>
-          </ToolsSection>
-          <ToolsSection title="Productivity">
-            <Tool title="Alfred">
-              Its not the newest kid on the block but its still the fastest. The
-              Sublime Text of the application launcher world.
-            </Tool>
-            <Tool title="Reflect">
-              Using a daily notes system instead of trying to keep things
-              organized by topics has been super powerful for me. And with
-              Reflect, its still easy for me to keep all of that stuff
-              discoverable by topic even though all of my writing happens in the
-              daily note.
-            </Tool>
-            <Tool title="SavvyCal">
-              Great tool for scheduling meetings while protecting my calendar
-              and making sure I still have lots of time for deep work during the
-              week.
-            </Tool>
-            <Tool title="Focus">
-              Simple tool for blocking distracting websites when I need to just
-              do the work and get some momentum going.
-            </Tool>
-          </ToolsSection>
         </div>
       </SimpleLayout>
     </>
