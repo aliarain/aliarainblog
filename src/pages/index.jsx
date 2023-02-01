63b6c1ff5d1f1e78b84a329ff9a6c7929aca7df2
Ali Arain <deistali2@gmail.com>
2023-02-01 15:41:50 +0500

    tets1

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -256,7 +256,8 @@ export default function Home({ articles }) {
       <Container className="mt-9">
         <div className="max-w-2xl">
           <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-            full stack developer, founder raptr42, and amateur astrophysicist.
+            full stack developer <br /> founder raptr42, and amateur
+            astrophysicist.
           </h1>
           <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
             I’m Ali, full stack developer and entrepreneur based in Pakistan.
