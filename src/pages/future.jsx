768d256d1ef1151fc3b4d5a9281b61c6075ed2cd
Ali Arain <deistali2@gmail.com>
2023-02-04 18:52:37 +0500

    a

--- src/pages/future.jsx
+++ src/pages/future.jsx
@@ -40,10 +40,13 @@ export default function Uses() {
       >
         <div className="space-y-10">
           <Tool title="raptr42">
-            at raptr, we are developing next-gen products for our clients. we
-            are building their mvp and making sure it works easy to use it for
-            the public and have a clear user experience.{' '}
-            <Link href="https://raptr42.com"> find more about us</Link>
+            at raptr, we are developing next-gen products for our clients.{' '}
+            <br /> we are building their mvp and making sure it works easy to
+            use it for the public and have a clear user experience.{' '}
+            <Link className="underline" href="https://raptr42.com">
+              {' '}
+              find more about us
+            </Link>
           </Tool>
           <Tool title="using ai to build better tools">
             this is my main focus after raptr42, im building useful tools to
