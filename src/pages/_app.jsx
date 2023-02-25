182a6e95705af99ba62ca6aaba1796f05bbe8416
Ali Arain <deistali2@gmail.com>
2023-02-25 23:32:15 +0500

    setup cms

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,8 +1,8 @@
-import { useEffect, useRef } from 'react'
+import { useEffect, useRef } from 'react';
 import { Analytics } from '@vercel/analytics/react';
 
-import { Footer } from '@/components/Footer'
-import { Header } from '@/components/Header'
+import { Footer } from '@/components/Footer';
+import { Header } from '@/components/Header';
 
 import '@/styles/tailwind.css'
 import 'focus-visible'
---
+++ src/pages/outstatic/[[...ost]].jsx
@@ -0,0 +1,8 @@
+import 'outstatic/outstatic.css'
+import { Outstatic, OstSSP } from 'outstatic'
+
+export function OutstaticPage(){
+    return Outstatic
+}
+
+export const getServerSideProps = OstSSP
