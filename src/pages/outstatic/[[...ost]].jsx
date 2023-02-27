8a74f9f793c30208cfe3ba969c74823853464315
Ali Arain <deistali2@gmail.com>
2023-02-27 23:18:04 +0500

    chnages in files

--- src/pages/outstatic/[[...ost]].jsx
+++
@@ -1,8 +0,0 @@
-import 'outstatic/outstatic.css'
-import { Outstatic, OstSSP } from 'outstatic'
-
-export function OutstaticPage(){
-    return Outstatic
-}
-
-export const getServerSideProps = OstSSP
